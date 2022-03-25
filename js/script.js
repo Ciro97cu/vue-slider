e5cd9d49142b62e047b26719cb05c12b81e8ed05
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-25 13:55:20 +0100

    loaded topic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+/* Descrizione:
+Partendo dal template fornito renderizzare lo slider con Vue
+Bonus:
+1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e
+farlo riprendere quando esce */
+
 const slides = [
     {
         image: 'img/01.jpg',
